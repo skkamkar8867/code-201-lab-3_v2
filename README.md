6b999ca36adc5b6732fa26f52387789a6e52798a
Simon Richard Eswaramoorthy <seswaram@amazon.com>
2019-02-22 16:37:51 -0800

    Lab4 Simon worked on Sushant's Lab 3

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # code-201-lab-3_v2
+
+Driver: Simon
+Navigator: Sushant
--- js/app.js
+++ js/app.js
@@ -1,116 +1,74 @@
-
-	var name = getname();
-	console.log(user);
-	alert('Welcome to Guessing game about me');
+"use strict";
+var correctflag;
+var name = getUserInput('What is your name?')
+alert('Welcome to Guessing game about me');
 var attempt = 0
-  var readyflag = getreadyflag();
-	checkreadyflag(readyflag);
-	var married = getmarried();
-	checkmarried(married);
-	var kid = getkid();
-	checkkid(kid);
-	var age = getage();
-	for (i = 0; i < 4; i++)
-	{checkage(age);
-	}
+var readyflag = getUserInput('Are you ready to play guessing game about me?')
+checkreadyflag(readyflag);
+var married = getUserInput('Am i married');
+checkmarried(married);
+var kid = getUserInput('Do you think i have kid');
+checkkid(kid);
+var age = getUserInput('Can you guess my age');
+checkage(age);
 
-	alert('Game Over!!!');
-
-
-/* The function gets name of the user */
-function getname() {
-	user = window.prompt('What is your name?');
-	return user;
-};
-
-
-/* The function prompts question to check national animal to user and returns the reply */
-function getreadyflag() {
-		readyflag = window.prompt('Are you ready to play guessing game about me?');
-		return readyflag;
-};
+alert('Game Over!!!');
 
 
 /* The function checks if user is interested in playing */
-function checkreadyflag(readyflag){
-	readyflag = this.readyflag;
+function checkreadyflag(readyflag) {
 	readyflag = readyflag.toLowerCase();
 	var expectedvalue1 = 'y';
-  var expectedvalue2 = 'yes';
+	var expectedvalue2 = 'yes';
 	if (readyflag === null) {
 		alert('No User Input. Thanking for Playing')
 	}
-	else if (readyflag === expectedvalue1 || readyflag === expectedvalue2)
-  {
+	else if (readyflag === expectedvalue1 || readyflag === expectedvalue2) {
 		alert('HOOOO!! Lets play the game');
 	}
 	else {
 		alert('Please enter yes or y to play game');
 	}
-};
+}
 
-/* The function prompts user to enter age */
-function getage() {
-	age = window.prompt('Can you guess my age');
-//checkage(age);
-alert("inside getage")
-  return age;
-};
 
 /* The function checks validates the age provided by user */
-function checkage(age){
-	age = this.age;
+function checkage(age) {
 	var correctage = '50';
- 
+
 	if (age === null) {
 		alert('No User Input. Thanking for Playing')
 	}
-  else if (age > 50) {
+	else if (age > 50) {
 		alert('Try lesser number')
 		verifyage();
 	}
-  else if (age < 50) {
+	else if (age < 50) {
 		alert('Try higher number')
 		verifyage();
 	}
 	else if (age === correctage) {
-		alert('Congratulations!! '+ age +' is correct answer.')
-    correctflag='Y';
+		alert('Congratulations!! ' + age + ' is correct answer.');
+		correctflag = 'Y';
 	}
 	else {
-		alert('Ops!! '+ age +' is incorrect answer.Please try the game again');
-	}
-	//attempt++;
-};
+		alert('Ops!! ' + age + ' is incorrect answer.Please try the game again');
+	}	
+}
 
-// function verifyage() {
-// 	alert("inside verify age");
-//   if (correctflag === 'Y' || attempt === 4)
-//     {return;}
-//   else
-//   getage();
-// }
-
-
-function getmarried() {
-	age = window.prompt('Am i married');
-	return married;
-};
-
-function getkid() {
-	age = window.prompt('Do you think i have kid');
-	return kid;
-};
-
+function verifyage() {
+	if (correctflag === 'Y' || attempt === 4) { return; }
+	else
+		getage();
+}
 
 
 
 /* The function checks validates the age provided by user */
-function checkmarried(married){
-	married = this.married;
-  	married = married.toLowerCase();
+function checkmarried(married) {
+	married = married.toLowerCase();
 	var expectedvalue3 = 'y';
-  var expectedvalue4 = 'yes';
+	var expectedvalue4 = 'yes';
 
 	if (married === null) {
 		alert('No User Input. Thanking for Playing')
@@ -121,11 +79,10 @@ function checkmarried(married){
 	else {
 		alert('Ops!! that is incorrect answer');
 	}
-};
+}
 
 /* The function checks validates the age provided by user */
-function checkkid(kid){
-	kid = this.kid;
+function checkkid(kid) {
 	var correctkid = 'y';
 	if (kid === null) {
 		alert('No User Input. Thanking for Playing')
@@ -136,4 +93,10 @@ function checkkid(kid){
 	else {
 		alert('Ops!! that is incorrect answer');
 	}
-};
+}
+
+
+function getUserInput(question) {
+	var answer = window.prompt(question);
+	return answer;
+}
